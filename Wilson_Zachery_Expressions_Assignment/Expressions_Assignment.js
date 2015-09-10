bd316590425cd9608f51907acaa229574b6245eb
zwilson85 <zrwilson@fullsail.edu>
2015-09-09 23:37:45 -0400

    Expression Assignments

---
+++ Wilson_Zachery_Expressions_Assignment/Expressions_Assignment.js
@@ -0,0 +1,47 @@
+/*
+Zach Wilson
+SDI Section #2
+Expressions Assignment
+9-9-15
+ */
+
+// Your age in Dog Years Calculator
+
+//Ask the user for their name
+var name = prompt("Whats your name?\nPlease type in your name.");
+
+//Use the variable and say hello
+alert("Welcome "+name+"!");
+
+//We need to know the current year
+//Hardcoded - means type in a specific value.
+var currentYear = 2015;
+
+//figure out age
+var age = currentYear - yearBorn
+
+//alert the user with their age
+alert(name+" you are "+age+" years old.");
+console.log(name+" you are "+age+" years old.");
+
+
+
+//Figure out a your age in dog years
+var dogYears= prompt("How old are you in dog years?");
+
+console.log(dogYears);
+
+//Current age * 7
+//Cast the variable as a number when using +
+var dogYears = age * 7 (dogYears) ;
+
+console.log(dogYears);
+
+//Console.log and alert the final values
+console.log("Your" +age+ * 7 , "you will be" +age+".");
+alert("In dog years, you will be"+age+".");
+
+
+//console.log the answer to check it
+console.log(age);
+
