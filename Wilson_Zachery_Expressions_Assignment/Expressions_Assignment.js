498559183ce077ee4b03f301cc8633d3e8787056
zwilson85 <zrwilson@fullsail.edu>
2015-09-10 00:00:00 -0400

    fixes

--- Wilson_Zachery_Expressions_Assignment/Expressions_Assignment.js
+++ Wilson_Zachery_Expressions_Assignment/Expressions_Assignment.js
@@ -13,27 +13,24 @@ var name = prompt("Whats your name?\nPlease type in your name.");
 //Use the variable and say hello
 alert("Welcome "+name+"!");
 
-//We need to know the current year
-//Hardcoded - means type in a specific value.
-var currentYear = 2015;
-
 //figure out age
-var age = currentYear - yearBorn
+var age = prompt("What is your age?");
+
+//console.log the answer to check it
+console.log(age);
 
 //alert the user with their age
 alert(name+" you are "+age+" years old.");
 console.log(name+" you are "+age+" years old.");
 
-
-
 //Figure out a your age in dog years
-var dogYears= prompt("How old are you in dog years?");
+var dogYears = prompt("How old are you in dog years?\nLets find out.");
 
 console.log(dogYears);
 
 //Current age * 7
-//Cast the variable as a number when using +
-var dogYears = age * 7 (dogYears) ;
+//Cast the variable as a number when using *
+var dogYears = age * 7;
 
 console.log(dogYears);
 
@@ -41,7 +38,6 @@ console.log(dogYears);
 console.log("Your" +age+ * 7 , "you will be" +age+".");
 alert("In dog years, you will be"+age+".");
 
-
 //console.log the answer to check it
 console.log(age);
-
+f
