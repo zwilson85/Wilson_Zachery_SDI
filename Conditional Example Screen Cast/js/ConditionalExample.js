497a10765c6963cdbffca6f3063656460ead9de0
zwilson85 <zrwilson@fullsail.edu>
2015-09-16 20:57:13 -0400

    Conditional Examples Screencast
    Conditional examples screencast

---
+++ Conditional Example Screen Cast/js/ConditionalExample.js
@@ -0,0 +1,18 @@
+/*
+ Zach Wilson
+ SDI Section #2
+ Conditional Example
+ 9/16/2015
+ */
+
+//Conditional Logic
+
+var oldEnough = false;
+
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+
+}
+console.log("what comes after");
