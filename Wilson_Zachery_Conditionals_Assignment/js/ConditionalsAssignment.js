1580facbf169a41fb41a56fba6b205300e6d6ed7
zwilson85 <zrwilson@fullsail.edu>
2015-09-17 22:31:26 -0400

    Created a calculator to figure out how many bills in the wallet.
    Helps user determine how many different bills in the digital wallet

---
+++ Wilson_Zachery_Conditionals_Assignment/js/ConditionalsAssignment.js
@@ -0,0 +1,44 @@
+ /*
+Zach Wilson
+SDI Section #2
+Conditionals Assignment
+9/16/2015
+ */
+
+alert("Test to see if connected.");
+
+//Create an array of US Currency. Question: You worked a side job for one week and earned $276.00, how many bills are in your digital wallet?
+
+//Ask the user for their name
+var name = prompt("Please type in your name.");
+
+//Use the variable and say hello
+alert("Hey There "+name+"!");
+
+//Ask the user the question
+alert("OK, here is your question "+name+"! You worked a side job for one week and earned $276.00 dollars, how many different bills are in your wallet?")
+
+var digitalWallet = ["Hundred Dollar Bill","Fifty Dollar Bill","Fifty Dollar Bill", "Twenty Dollar Bill","Twenty Dollar Bill","Twenty Dollar Bill","Ten Dollar Bill","Five Dollar Bill","One Dollar Bill"];
+
+//See Complete Array
+console.log(digitalWallet);
+
+//Console.log the Hundred Dollar Bill(s).
+console.log(digitalWallet[1]);
+
+//Console.log the Fifty Dollar Bill(s).
+console.log(digitalWallet[2]);
+
+//Console.log the Twenty Dollar Bill(s).
+console.log(digitalWallet[3]);
+
+//Console.log the Ten Dollar Bill(s).
+console.log(digitalWallet[1]);
+
+//Console.log the Five Dollar Bill(s).
+console.log(digitalWallet[1]);
+
+//Console.log the One Dollar Bill(s).
+console.log(digitalWallet[1]);
+
+//
